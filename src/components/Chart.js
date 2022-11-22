ddbf0c2641c234c3800f1ede7aad635e9bdb8de1
Joey Southerland <31213840+jcsoutherland@users.noreply.github.com>
2022-11-22 12:53:01 -0800

    refactored project for react

---
+++ src/components/Chart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Chart = () => {
+  return <div>chart</div>
+}
+
+const domContainer = document.querySelector('#parent-container')
+const root = ReactDOM.createRoot(domContainer)
+root.render(e(Chart))
+export default Chart
